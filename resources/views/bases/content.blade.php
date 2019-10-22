fc1c2e7c6f2ccfa3867b0058ed4daab28c585fa4
Celeste Magisteel <kamg1134@gmail.com>
2019-10-22 13:18:40 +0100

    Adjusted Content Template to use % padding instead of fixed values

--- resources/views/bases/content.blade.php
+++ resources/views/bases/content.blade.php
@@ -2,8 +2,8 @@
 
 @section('body')
     <p></p>
-    <div id="container" style="padding-left: 150px; padding-right: 150px">
-        <div id="content" class="bg-light text-dark" style="padding: 25px 100px; border: 5px solid black; border-radius: 25px;">
+    <div id="container" style="padding-left: 5%; padding-right: 5%">
+        <div id="content" class="bg-light text-dark" style="padding: 1% 100px; border: 5px solid black; border-radius: 25px;">
             @yield('content')
         </div>
     </div>
