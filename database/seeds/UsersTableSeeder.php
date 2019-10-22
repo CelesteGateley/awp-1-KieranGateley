1f3e1d71f6b30af42a9ca79683390ca5129231c5
Celeste Magisteel <kamg1134@gmail.com>
2019-10-22 12:43:04 +0100

    Implemented PostFactory, and changed UsersTableSeeder to seed user posts at the same time

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -1,12 +1,16 @@
 <?php
 
 use App\User;
+use App\Post;
 use Illuminate\Database\Seeder;
 
 class UsersTableSeeder extends Seeder {
 
     public function run() {
-        factory(User::class, 20)->create();
+        factory(User::class, 20)->create()->each(function ($user) {
+            $posts = factory(Post::class, 3)->make();
+            $user->posts()->saveMany($posts);
+        });
     }
 
 }
