56aa9584ca384bad62324699533cddf358473af3
Celeste Magisteel <kamg1134@gmail.com>
2019-10-25 15:50:57 +0100

    Implemented Article Row Template

---
+++ resources/views/parts/blog/row.blade.php
@@ -0,0 +1,8 @@
+<tr>
+    <th scope="row">{{ $id }}</th>
+    <td>{{ $author }}</td>
+    <td>{{ $title }}</td>
+    <td>{{ $post }}</td>
+    <td>{{ $created_on }}</td>
+    <td>{{ $updated_on }}</td>
+</tr>
