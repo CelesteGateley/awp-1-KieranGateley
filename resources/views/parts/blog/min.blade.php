ae7f6562e3cefa96fb43219812b051c745130ae8
Celeste Magisteel <kamg1134@gmail.com>
2019-10-25 10:36:28 +0100

    Removed unused code from minimal post template

--- resources/views/parts/blog/min.blade.php
+++ resources/views/parts/blog/min.blade.php
@@ -3,12 +3,10 @@
     <div id="body">{{ $body }}</div>
     <div id="footer" class="border-top">
         <div id="info" class="text-left" style="width: 70%; display: inline-block">
-            Written by {{ $author }} <!--on {{ #article.createdOn|date("Y-m-d") }} at {{ #article.createdOn|date("H:i") }}-->
+            Written by {{ $author }}
         </div>
         <div class="text-right" style="width: 29%; display: inline-block">
-            <a href="/article/{{ article.id }}">
                 Read more...
-            </a>
         </div>
     </div>
 </div>
