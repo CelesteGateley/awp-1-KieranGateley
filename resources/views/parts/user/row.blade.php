c9a0e22a6a3d66586240b7088dda7fa99fb40c39
Celeste Magisteel <kamg1134@gmail.com>
2019-10-25 15:55:16 +0100

    Implemented User Row Part

---
+++ resources/views/parts/user/row.blade.php
@@ -0,0 +1,8 @@
+<tr>
+    <th scope="row">{{ $id }}</th>
+    <td>{{ $email }}</td>
+    <td>{{ $name }}</td>
+    <td>{{ $verified_on }}</td>
+    <td>{{ $joined_on }}</td>
+    <td>{{ $updated_on }}</td>
+</tr>
