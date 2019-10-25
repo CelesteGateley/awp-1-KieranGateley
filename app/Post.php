7c16c87e7e72dba6c2ee8a5ec77c1101fb3349a8
Celeste Magisteel <kamg1134@gmail.com>
2019-10-25 10:33:20 +0100

    Added method to post to get the name of the author

--- app/Post.php
+++ app/Post.php
@@ -9,4 +9,9 @@ class Post extends Model
     public function poster() {
         $this->belongsTo('App/User');
     }
+
+    public function author() {
+        $user = User::find($this->user_id);
+        return $user->name;
+    }
 }
