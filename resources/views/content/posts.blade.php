ecb07e9f849dca32d35f05adaa3e9f920d29b672
Celeste Magisteel <kamg1134@gmail.com>
2019-10-25 16:11:40 +0100

    Fixed undefined variable ID and changed table style

--- resources/views/content/posts.blade.php
+++ resources/views/content/posts.blade.php
@@ -3,19 +3,18 @@
 @section('title', 'All Posts')
 
 @section('content')
-    <table>
-        <thead>
+    <table style="width: 100%;" class="table">
+        <thead class="thead-dark">
         <tr>
             <th scope="col">Post ID</th>
             <th scope="col">Author</th>
             <th scope="col">Title</th>
-            <th scope="col">Content</th>
             <th scope="col">Created On</th>
             <th scope="col">Last Updated</th>
         </tr>
         </thead>
     @foreach ($posts as $post)
-        @include('parts.blog.row', ['title' => $post->title, 'body' => $post->post, 'author' => $post->author(), 'created_on' => $post->created_at, 'updated_on' => $post->updated_at, ])
+        @include('parts.blog.row', ['id' => $post->id, 'title' => $post->title, 'body' => $post->post, 'author' => $post->author(), 'created_on' => $post->created_at, 'updated_on' => $post->updated_at, ])
     @endforeach
     </table>
 @endsection
--- resources/views/parts/blog/row.blade.php
+++ resources/views/parts/blog/row.blade.php
@@ -2,7 +2,6 @@
     <th scope="row">{{ $id }}</th>
     <td>{{ $author }}</td>
     <td>{{ $title }}</td>
-    <td>{{ $post }}</td>
     <td>{{ $created_on }}</td>
     <td>{{ $updated_on }}</td>
 </tr>
