85d2c599ad4a88641432247367f836fe469b8a1a
Celeste Magisteel <kamg1134@gmail.com>
2019-10-28 14:16:29 +0000

    Implemented basic permission setup, and added static root user for testing

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,10 @@
 <?php
 
+use App\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +15,28 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $this->static_data();
         $this->call(UsersTableSeeder::class);
+    }
+
+    private function static_data() {
+        $this->roles();
+        $root_user = User::create([
+            'name' => 'Root Admin',
+            'email' => 'root@localhost',
+            'email_verified_at' => now(),
+            'password' => Hash::make('root_admin_password')
+        ]);
+
+        $root_user->save();
+        $root_user->assignRole('root');
+    }
+
+    private function roles() {
+        $root_role = Role::create(['name' => 'root',]);
+        $manage_post_permission = Permission::create(['name' => 'manage_posts',]);
+        $manage_user_permission = Permission::create(['name' => 'manage_users',]);
+        $root_role->givePermissionTo($manage_post_permission);
+        $root_role->givePermissionTo($manage_user_permission);
     }
 }
--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -3,10 +3,12 @@
 use App\User;
 use App\Post;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class UsersTableSeeder extends Seeder {
 
     public function run() {
+
         factory(User::class, 20)->create()->each(function ($user) {
             $posts = factory(Post::class, 3)->make();
             $user->posts()->saveMany($posts);
