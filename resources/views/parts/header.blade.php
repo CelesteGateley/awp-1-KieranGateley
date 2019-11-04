35bd1ce945de7d0207e5444666d8541e73da3482
Celeste Magisteel <kamg1134@gmail.com>
2019-11-04 10:25:55 +0000

    Adjusted navbar to show logout button

--- resources/views/parts/header.blade.php
+++ resources/views/parts/header.blade.php
@@ -5,13 +5,22 @@
     </button>
 
     <div class="navbar-collapse collapse" id="collapsingNavbar">
+        @if (Auth::check())
         <ul class="nav navbar-nav ml-auto">
             <li class="nav-item">
-                <a class="nav-link" href="/login">Login</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="/register">Register</a>
+                <a class="nav-link" href="{{ url('/logout') }}" onclick="event.preventDefault();document.getElementById('logout-form').submit();">Logout</a>
             </li>
         </ul>
+        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;"> {{ csrf_field() }} </form>
+        @else
+        <ul class="nav navbar-nav ml-auto">
+            <li class="nav-item">
+                <a class="nav-link" href="{{ url('/login') }}">Login</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="{{ url('/register') }}">Register</a>
+            </li>
+        </ul>
+        @endif
     </div>
 </nav>
