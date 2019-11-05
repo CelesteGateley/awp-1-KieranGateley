97430ffd681b89eb9b5564c20aa3f50ad651e332
Celeste Magisteel <kamg1134@gmail.com>
2019-11-05 14:07:00 +0000

    Updated PostFactory to seed multiple sentences

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -8,6 +8,6 @@ use Faker\Generator as Faker;
 $factory->define(Post::class, function (Faker $faker) {
     return [
         "title" => $faker->sentence(),
-        "post" => implode(' ', $faker->sentences),
+        "post" => implode(' ', $faker->sentences($nb = 20)),
     ];
 });
