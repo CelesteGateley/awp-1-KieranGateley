d8251c9845952efbeb46884c96ea470094c49d5b
Celeste Magisteel <kamg1134@gmail.com>
2019-11-05 14:57:29 +0000

    limited the amount of words to be shown in the min post

--- resources/views/parts/blog/min.blade.php
+++ resources/views/parts/blog/min.blade.php
@@ -1,6 +1,6 @@
 <div class="post" style="border: 2px solid black; border-radius: 5px; padding-left: 5px; padding-right: 5px">
     <div id="header" class="border-bottom"><b>{{ $title }}</b></div>
-    <div id="body">{{ $body }}</div>
+    <div id="body">{{ Str::words($body, $limit=50, $end='...') }}</div>
     <div id="footer" class="border-top">
         <div id="info" class="text-left" style="width: 70%; display: inline-block">
             Written by <a href="#">{{ $author }}</a> on {{ $date }}
