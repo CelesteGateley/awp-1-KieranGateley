11419b3a1edfb25b592336a06138feb6336d2fee
Celeste Magisteel <kamg1134@gmail.com>
2019-11-05 14:14:15 +0000

    Fixed misnamed variable in max post

--- resources/views/parts/blog/max.blade.php
+++ resources/views/parts/blog/max.blade.php
@@ -1,6 +1,6 @@
 <div id="title" class="border-bottom">
-    <h1 style="display: inline-block;">{{ $title }}</h1><em style="font-size: 12px; display: inline-block">Written by {{ $author }} on {{ $date }}</em>
+    <h1 style="display: inline-block;">{{ $title }}</h1>&nbsp<em style="font-size: 12px; display: inline-block">Written by {{ $author }} on {{ $date }}</em>
 </div>
 <div id="body">
-    {{ $post }}
+    {{ $body }}
 </div>
