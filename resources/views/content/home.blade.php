6f3a7b7d37c51cb54c77eb7363c8f5f60a7e948d
Celeste Magisteel <kamg1134@gmail.com>
2019-11-05 14:58:02 +0000

    Switched posts to pagination for easier consumption, and changed from ordering by id to ordering by latest

--- resources/views/content/home.blade.php
+++ resources/views/content/home.blade.php
@@ -7,4 +7,5 @@
         @include('parts.blog.min', ['id' => $post->id, 'title' => $post->title, 'body' => $post->post, 'author' => $post->author(), 'date' => $post->created_at->format('l jS F Y \\a\\t h:iA'), ])
         <p></p>
     @endforeach
+    {{ $posts->links() }}
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('content.home', ['posts' => App\Post::all()]);
+    return view('content.home', ['posts' => App\Post::latest()->paginate(10), ]);
 })->name('home');
 
 Route::get('/posts', function () {
