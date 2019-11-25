d866e9b4ca63d53ccfcf8a864c087bede93a95ab
Celeste Magisteel <kamg1134@gmail.com>
2019-11-25 11:48:15 +0000

    Made is_administrator and is_root default to false

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,8 +19,8 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->boolean('is_administrator');
-            $table->boolean('is_root');
+            $table->boolean('is_administrator')->default(false);
+            $table->boolean('is_root')->default(false);
             $table->rememberToken();
             $table->timestamps();
         });
