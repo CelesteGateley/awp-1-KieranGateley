a254ca1e19679f0bc2431aa783f3a2e90640127b
Celeste Magisteel <kamg1134@gmail.com>
2019-11-25 12:32:31 +0000

    Added better checking to delete button

--- resources/views/parts/blog/row.blade.php
+++ resources/views/parts/blog/row.blade.php
@@ -4,7 +4,9 @@
     <td>{{ $title }}</td>
     <td>{{ $created_on }}</td>
     <td>{{ $updated_on }}</td>
-    @if(Auth::check())
+    @if(Auth::check() && (Auth::user()->is_administrator || Auth::user()->is($post->poster)))
         <td><a href="{{route('delete_post', ['post' => $post,])}}"><ion-icon name="trash"></ion-icon></a></td>
+    @elseif(Auth::check())
+        <td><ion-icon name="trash"></ion-icon></td>
     @endif
 </tr>
