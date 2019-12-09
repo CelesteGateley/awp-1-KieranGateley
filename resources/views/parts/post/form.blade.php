5354520a5b99fd07c4631df60a82ef9597b9b7ba
Celeste Magisteel <kamg1134@gmail.com>
2019-12-09 10:21:20 +0000

    Fixed an issue with the form not displaying the body of a post

--- resources/views/parts/post/form.blade.php
+++ resources/views/parts/post/form.blade.php
@@ -2,10 +2,12 @@
     <label for="title-input">Post Title</label>
     <input name="title" value="{{ $title }}" class="form-control" id="title-input">
 </div>
+<p></p>
 <div id="form-group">
     <label for="title-input">Body</label>
-    <textarea name="title" value="{{ $body }}" class="form-control" id="body-input" rows="5"></textarea>
+    <textarea name="title" class="form-control" id="body-input" rows="5">{{ $body }}</textarea>
 </div>
+<p></p>
 <div id="form-group">
-    <button type="submit" class="btn btn-primary">Create New Post</button>
+    <button type="submit" class="btn btn-primary">Submit</button>
 </div>
