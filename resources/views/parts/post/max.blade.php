a468d72ef0182198baea9b189efe734509006053
CelesteMagisteel <celestemagisteel@gmail.com#>
2020-01-13 16:10:28 +0000

    Added space after title on max post

--- resources/views/parts/post/max.blade.php
+++ resources/views/parts/post/max.blade.php
@@ -2,5 +2,6 @@
     <h1 style="display: inline-block;">{{ $title }}</h1>&nbsp<em style="font-size: 12px; display: inline-block">Written by {{ $author }} on {{ $date }}</em>
 </div>
 <div id="body">
+    <br />
     {{ $body }}
 </div>
