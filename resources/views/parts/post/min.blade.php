789cc3f4e25cbb58bf5ff907bf0cd72eb0d1926d
CelesteMagisteel <celestemagisteel@gmail.com#>
2020-01-13 16:10:05 +0000

    Removed redundant link on min post

--- resources/views/parts/post/min.blade.php
+++ resources/views/parts/post/min.blade.php
@@ -3,7 +3,7 @@
     <div id="body">{{ Str::words($body, $limit=50, $end='...') }}</div>
     <div id="footer" class="border-top">
         <div id="info" class="text-left" style="width: 70%; display: inline-block">
-            Written by <a href="#">{{ $author }}</a> on {{ $date }}
+            Written by {{ $author }} on {{ $date }}
         </div>
         <div class="text-right" style="width: 29%; display: inline-block">
             <a href="{{ route('post', ['post' => $post, ]) }}">Read more...</a>
